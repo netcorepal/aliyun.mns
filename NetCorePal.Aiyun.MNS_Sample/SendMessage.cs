67097d24948fff62263f7d9915f1b3a3ad774348
wangdaobin <daobin.wang@xiaobao100.com>
2018-01-05 18:43:35 +0800

    修改同步方法异常处理

--- NetCorePal.Aiyun.MNS_Sample/SendMessage.cs
+++ NetCorePal.Aiyun.MNS_Sample/SendMessage.cs
@@ -16,31 +16,42 @@ namespace NetCorePal.Aiyun.MNS_Sample
             var config = Newtonsoft.Json.JsonConvert.DeserializeObject<ConfigModel>(File.ReadAllText(@"E:\MNS.json"));
 
             IMNS client = new Aliyun.MNS.MNSClient(config.AccessKeyId, config.AccessKey, config.EndPoint, null);
+            //var index = 1;
+            //while (index<=100)
+            //{
+            //    var queue=client.CreateQueueAsync("testQueue").Result;
+            //    queue.SendMessageAsync($"Hello Message").Wait();
+            //    client.DeleteQueueAsync("testQueue").Wait();
+            //    index++;
+            //}
+            //client.CreateQueueAsync("testQueue").ContinueWith( async task=>
+            //{
 
-            client.CreateQueueAsync("testQueue").ContinueWith( async task=>
-            {
-                Stopwatch stopWatch = new Stopwatch();
-                int index = 1;
-                stopWatch.Start();
-                while (index<=10)
-                {
-                    task.Result.SendMessageAsync($"Hello Message {index}");
+            //    await task.Result.SendMessageAsync($"Hello Message");
+            //    //Stopwatch stopWatch = new Stopwatch();
+            //    //int index = 1;
+            //    //stopWatch.Start();
+            //    //while (index<=10)
+            //    //{
+            //    //    task.Result.SendMessageAsync($"Hello Message {index}");
 
-                    index++;
-                }
-                stopWatch.Stop();
-                TimeSpan ts = stopWatch.Elapsed;
-                string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
-                                                       ts.Hours, ts.Minutes, ts.Seconds,
-                                                       ts.Milliseconds / 10);
-                Console.WriteLine("RunTime " + elapsedTime);
-                Console.WriteLine("跑完消息");
+            //    //    index++;
+            //    //}
+            //    //stopWatch.Stop();
+            //    //TimeSpan ts = stopWatch.Elapsed;
+            //    //string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+            //    //                                       ts.Hours, ts.Minutes, ts.Seconds,
+            //    //                                       ts.Milliseconds / 10);
+            //    //Console.WriteLine("RunTime " + elapsedTime);
+            //    //Console.WriteLine("跑完消息");
 
-                await client.DeleteQueueAsync("testQueue");
-            });
+            //    await client.DeleteQueueAsync("testQueue");
+
+            //    await client.CreateQueueAsync("testQueue");
+            //});
 
 
-
+            Console.WriteLine("结束");
             Console.ReadKey();
         }
     }
